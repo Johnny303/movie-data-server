031fe0160b26cd634039dde7ac90a96cf49c15d9
Nikola Zrnic <zrnic.nikola@outlook.com>
2023-08-01 21:24:58 +0200

    done search the movie. TODO make it work for multi words

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 import fs from "fs";
+import { title } from "process";
 
 app.listen(3000, () => {
     console.log(`Open this link in your browser: http://127.0.0.1:3000`);
@@ -10,9 +11,7 @@ app.get('/api/movies/all', (req, res) => {
     fs.readFile('./data.json', 'utf8', (err, data) => {
         if (err) throw err;
         const movies = JSON.parse(data).movies;
-        
         let movieTitles  = []
-
         movies.forEach(movie => {
             movieTitles.push(movie.title)
         })
@@ -20,3 +19,46 @@ app.get('/api/movies/all', (req, res) => {
         res.send(movieTitles);
 });
 });
+app.get('/api/movies/title/:search', (req, res) => {
+    fs.readFile('./data.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const movies = JSON.parse(data).movies;
+        const {search} = req.params
+        let movieToDisplay = []
+
+        movies.forEach(movie => {
+            if (movie.title === search){
+                movieToDisplay.push(movie)
+            }
+        })
+       console.log(movieToDisplay)
+       res.send(movieToDisplay)
+    });
+});
+    //TODO make it work for multy words
+
+
+
+
+
+
+
+
+
+// app.get('/api/movies/title/:titlesearch', (req,res) => {
+//     fs.readFile('./data.json', 'utf-8', (err,data) => {
+//         if (err) throw err
+//         const movies = JSON.parse(data)
+//         const {titlesearch} = req.params.titlesearch
+//         let movieToDisplay = []
+
+//         console.log(movies)
+
+        
+//         if (movieToDisplay){
+//             res.send(movieToDisplay)
+//         } else {
+//             res.status(404).send({state: "User Not Found"})
+//         }
+//     })
+// })
